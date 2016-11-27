ad21baf792cbcb7d9817cfa02f3cf39434ef89ef
Michael Fry-White <michaelfry2002@gmail.com>
2016-11-27 17:26:50 +0000

    Swagger it uuupppp!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,73 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { VegerunClient } from './vegerun-client';
+
 import { MOBILE } from './services/constants';
 
 const views: Object[] = [
-  {
-    name: 'Dashboard',
-    icon: 'home',
-    link: ['']
-  },
-  {
-    name: 'Lazy',
-    icon: 'file_download',
-    link: ['lazy']
-  },
     {
-    name: 'Sync',
-    icon: 'done',
-    link: ['sync']
-  },
-  {
-    name: 'Bad Link',
-    icon: 'error',
-    link: ['wronglink']
-  }
+        name: 'Dashboard',
+        icon: 'home',
+        link: ['']
+    },
+    {
+        name: 'Lazy',
+        icon: 'file_download',
+        link: ['lazy']
+    },
+    {
+        name: 'Sync',
+        icon: 'done',
+        link: ['sync']
+    },
+    {
+        name: 'Bad Link',
+        icon: 'error',
+        link: ['wronglink']
+    }
 ];
 
 @Component({
-  selector: 'my-app',
-  styleUrls: ['./app.component.css'],
-  templateUrl: './app.component.html'
+    selector: 'my-app',
+    styleUrls: ['./app.component.css'],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  showMonitor = (ENV === 'development' && !AOT &&
-    ['monitor', 'both'].includes(STORE_DEV_TOOLS) // set in constants.js file in project root
-  );
-  mobile = MOBILE;
-  sideNavMode = MOBILE ? 'over' : 'side';
-  views = views;
+    showMonitor = (ENV === 'development' && !AOT &&
+        ['monitor', 'both'].includes(STORE_DEV_TOOLS) // set in constants.js file in project root
+    );
+    mobile = MOBILE;
+    sideNavMode = MOBILE ? 'over' : 'side';
+    views = views;
 
-  constructor(
-    public route: ActivatedRoute,
-    public router: Router
-  ) { }
-
-  activateEvent(event) {
-    if (ENV === 'development') {
-      console.log('Activate Event:', event);
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private vegerunClient: VegerunClient
+    )
+    {
+        vegerunClient.apiV1LocationsGetByPostcodeGet('EC2A2EX')
+            .flatMap(location => vegerunClient.apiV1RestaurantsSearchSearchPost({
+                latitude: location.position.latitude,
+                longitude: location.position.longitude,
+                townId: location.town.id,
+                postcode: location.normalizedPostcode
+            }))
+            .subscribe(results => {
+                console.log('got results!' + results);
+            });
     }
-  }
 
-  deactivateEvent(event) {
-    if (ENV === 'development') {
-      console.log('Deactivate Event', event);
+    activateEvent(event) {
+        if (ENV === 'development') {
+            console.log('Activate Event:', event);
+        }
     }
-  }
+
+    deactivateEvent(event) {
+        if (ENV === 'development') {
+            console.log('Deactivate Event', event);
+        }
+    }
 }
--- src/app/constants/app.providers.ts
+++ src/app/constants/app.providers.ts
@@ -1,7 +1,14 @@
+import { Provider, ValueProvider } from '@angular/core';
+
+import { API_BASE_URL, VegerunClient } from '../vegerun-client';
+
 import { UserActions } from '../user/user.actions';
 import { UserService } from '../user/user.service';
 
-export const APP_PROVIDERS = [
-  UserActions,
-  UserService
+export const APP_PROVIDERS: Provider[] = [
+    UserActions,
+    UserService,
+
+    { provide: API_BASE_URL, useValue: 'http://localhost:5000' } as ValueProvider,
+    VegerunClient
 ];
